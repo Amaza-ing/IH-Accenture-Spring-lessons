e60d80094c72430556a278915c6aaa413dbde43d
Adrián Maza <>
2023-04-27 09:14:33 +0200

    w3d4 created

---
+++ w3d4/src/main/java/com/ironhack/w3d4/model/MultipleChoiceExam.java
@@ -0,0 +1,28 @@
+package com.ironhack.w3d4.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+
+import java.util.Date;
+
+@Entity
+@PrimaryKeyJoinColumn(name = "id")
+public class MultipleChoiceExam extends Exam{
+    private Integer numberOfChoices;
+
+    public MultipleChoiceExam() {
+    }
+
+    public MultipleChoiceExam(String id, Date startDate, Boolean mandatory, Integer numberOfChoices) {
+        super(id, startDate, mandatory);
+        this.numberOfChoices = numberOfChoices;
+    }
+
+    public Integer getNumberOfChoices() {
+        return numberOfChoices;
+    }
+
+    public void setNumberOfChoices(Integer numberOfChoices) {
+        this.numberOfChoices = numberOfChoices;
+    }
+}
---
+++ w3d4/src/main/java/com/ironhack/w3d4/model/Teacher.java
@@ -0,0 +1,46 @@
+package com.ironhack.w3d4.model;
+
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name = "teacher") // En caso de que la tabla se llame de manera diferente a la clase
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id") // En caso de que la propiedad se llame de manera diferente al campo
+    private Integer id;
+
+//    @Column(name = "teacher")  // En caso de que la propiedad se llame de manera diferente al campo
+    private String teacher;
+
+    public Teacher() {
+    }
+
+    public Teacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", teacher='" + teacher + '\'' +
+                '}';
+    }
+}
