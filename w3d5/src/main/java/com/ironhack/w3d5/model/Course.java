ff2cf2e3b748a5824ad74c2d59c0a140567181eb
Adrián Maza <>
2023-04-28 10:22:43 +0200

    w3d5 lombok

--- w3d5/src/main/java/com/ironhack/w3d5/model/Course.java
+++ w3d5/src/main/java/com/ironhack/w3d5/model/Course.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
     @Id
     private String course;
@@ -17,65 +23,4 @@ public class Course {
     @ManyToOne
     @JoinColumn(name = "teacher_id")
     private Teacher teacher;
-
-    public Course() {
-    }
-
-    public Course(String course, Integer hours, String classroom, String vacations, Teacher teacher) {
-        this.course = course;
-        this.hours = hours;
-        this.classroom = classroom;
-        this.vacations = vacations;
-        this.teacher = teacher;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public Integer getHours() {
-        return hours;
-    }
-
-    public void setHours(Integer hours) {
-        this.hours = hours;
-    }
-
-    public String getClassroom() {
-        return classroom;
-    }
-
-    public void setClassroom(String classroom) {
-        this.classroom = classroom;
-    }
-
-    public String getVacations() {
-        return vacations;
-    }
-
-    public void setVacations(String vacations) {
-        this.vacations = vacations;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
-    @Override
-    public String toString() {
-        return "Course{" +
-                "course='" + course + '\'' +
-                ", hours=" + hours +
-                ", classroom='" + classroom + '\'' +
-                ", vacations='" + vacations + '\'' +
-                '}';
-    }
 }
--- w3d5/src/main/java/com/ironhack/w3d5/model/Teacher.java
+++ w3d5/src/main/java/com/ironhack/w3d5/model/Teacher.java
@@ -1,8 +1,14 @@
 package com.ironhack.w3d5.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,55 +22,12 @@ public class Teacher {
 //    Sólo utilizar este mapeo si queremos una relación bidireccional
 //    Si lo utilizamos, hay que tener cuidado de no ocasionar un error de tipo stackOverflow (bucle infinito)
 //    @OneToMany(mappedBy = "teacher")
+//    @ToString.Exclude
 //    private List<Course> courses;
 
-    public Teacher() {
-    }
 
     public Teacher(String teacher, Address address) {
         this.teacher = teacher;
         this.address = address;
-    }
-
-    public Integer getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(Integer teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public String getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(String teacher) {
-        this.teacher = teacher;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-//    public List<Course> getCourses() {
-//        return courses;
-//    }
-//
-//    public void setCourses(List<Course> courses) {
-//        this.courses = courses;
-//    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "teacherId=" + teacherId +
-                ", teacher='" + teacher + '\'' +
-                ", address=" + address +
-//                ", courses=" + courses +
-                '}';
     }
 }
