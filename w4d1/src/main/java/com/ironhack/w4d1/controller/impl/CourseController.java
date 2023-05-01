569b4f3b9238261c14330ba1747518991b2d4462
Adrián Maza <>
2023-05-01 12:15:06 +0200

    w4d1 completed

---
+++ w4d1/src/main/java/com/ironhack/w4d1/controller/impl/CourseController.java
@@ -0,0 +1,2 @@
+package com.ironhack.w4d1.controller.impl;public class CourseController {
+}
---
+++ w4d1/src/main/java/com/ironhack/w4d1/controller/interfaces/ITeacherController.java
@@ -0,0 +1,2 @@
+package com.ironhack.w4d1.controller.interfaces;public interface ITeacherController {
+}
