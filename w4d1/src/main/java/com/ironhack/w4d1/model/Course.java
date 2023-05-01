5726fc7bc6387f3c9b5fce4d79e3f23a0361004e
Adrián Maza <>
2023-05-01 09:23:49 +0200

    w4d1 created

---
+++ w4d1/src/main/java/com/ironhack/w4d1/model/Course.java
@@ -0,0 +1,26 @@
+package com.ironhack.w4d1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    private String course;
+
+    private Integer hours;
+    private String classroom;
+    private String vacations;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+}
---
+++ w4d1/src/test/java/com/ironhack/w4d1/W3d2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.w4d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3d2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
